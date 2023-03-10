e97cc6bd30ae76a840c01442d14472b8ab378e12
christianarca27 <christian.arca.mail@gmail.com>
2023-03-10 11:05:02 +0100

    refactor: ordinato script + commenti script e style

--- js/script.js
+++ js/script.js
@@ -22,91 +22,84 @@ const images = [
     }
 ];
 
-const carouselEl = document.getElementById("carousel");
 
+
+// Inizializzo la struttura principale del carosello
+const carouselEl = document.getElementById("carousel");
+// Inizializzo la struttura principale della main-picture
 const mainPictureEl = document.createElement("div");
 mainPictureEl.id = "main-picture";
 carouselEl.append(mainPictureEl);
+// Inizializzo l'immagine della main-picture
+const mainPictureImageEl = document.createElement("img");
+mainPictureImageEl.id = "image";
+mainPictureEl.append(mainPictureImageEl);
+// Inizializzo il campo descrizione della main-picture
+const mainPictureDescriptionEl = document.createElement("div");
+mainPictureDescriptionEl.id = "description";
+mainPictureEl.append(mainPictureDescriptionEl);
+// Inizializzo il titolo della main-picture
+const mainPictureTitleEl = document.createElement("div");
+mainPictureTitleEl.id = "title";
+mainPictureDescriptionEl.append(mainPictureTitleEl);
+// Inizializzo il testo descrittivo della main-picture
+const mainPictureTextEl = document.createElement("div");
+mainPictureTextEl.id = "text";
+mainPictureDescriptionEl.append(mainPictureTextEl);
 
+
+
+// Inizializzo la struttura che contiene le thumbnails
+const thumbnailsEl = document.createElement("div");
+thumbnailsEl.id = "thumbnails";
+carouselEl.append(thumbnailsEl);
+// Inizializzo la lista delle thumbnails
+const thumbnailsList = [];
+for(let i = 0; i < images.length; i++) {
+    // Creo e stilizzo la singola thumbnail
+    const thumbnailEl = document.createElement("div");
+    thumbnailEl.classList.add("thumbnail");
+    thumbnailEl.style.height = "calc(100% / " + images.length + ")";
+    thumbnailsEl.append(thumbnailEl);
+    // Creo l'immagine contenuta nella thumbnail
+    const thumbnailImageEl = document.createElement("img");
+    thumbnailImageEl.src = "../" + images[i].image;
+    thumbnailEl.append(thumbnailImageEl);
+    // Al click sulla thumbnail diventa la main-picture
+    thumbnailEl.addEventListener("click", () => {
+        actualIndex = i;
+        showImageByIndex();
+    });
+    // Aggiungo la thumbnail creata alla lista di thumbnails
+    thumbnailsList.push(thumbnailEl);
+}
+
+
+
+// Creo il bottone per passare all'immagine precedente
+const controlPrevEl = document.createElement("i");
+controlPrevEl.id = "control-prev";
+controlPrevEl.className = "fa-solid fa-circle-chevron-up fa-2xl";
+thumbnailsEl.append(controlPrevEl);
+controlPrevEl.addEventListener("click", showPrevImage);
+
+
+
+// Creo il bottone per passare all'immagine succesiva
+const controlNextEl = document.createElement("i");
+controlNextEl.id = "control-next";
+controlNextEl.className = "fa-solid fa-circle-chevron-down fa-2xl";
+thumbnailsEl.append(controlNextEl);
+controlNextEl.addEventListener("click", showNextImage);
+
+
+
+// Creo il bottone per avviare lo switch automatico del carosello ogni 3 secondi
 const controlStartEl = document.createElement("i");
 controlStartEl.id = "control-start";
 controlStartEl.className = "fa-solid fa-circle-play fa-2xl";
 controlStartEl.style.display = "none";
 mainPictureEl.append(controlStartEl);
-
-const controlStopEl = document.createElement("i");
-controlStopEl.id = "control-stop";
-controlStopEl.className = "fa-solid fa-circle-pause fa-2xl";
-mainPictureEl.append(controlStopEl);
-
-const controlChangeEl = document.createElement("i");
-controlChangeEl.id = "control-change";
-controlChangeEl.className = "fa-solid fa-arrows-rotate fa-2xl";
-mainPictureEl.append(controlChangeEl);
-
-const mainPictureImageEl = document.createElement("img");
-mainPictureImageEl.id = "image";
-mainPictureEl.append(mainPictureImageEl);
-
-const mainPictureDescriptionEl = document.createElement("div");
-mainPictureDescriptionEl.id = "description";
-mainPictureEl.append(mainPictureDescriptionEl);
-
-const mainPictureTitleEl = document.createElement("div");
-mainPictureTitleEl.id = "title";
-mainPictureDescriptionEl.append(mainPictureTitleEl);
-
-const mainPictureTextEl = document.createElement("div");
-mainPictureTextEl.id = "text";
-mainPictureDescriptionEl.append(mainPictureTextEl);
-
-const thumbnailsEl = document.createElement("div");
-thumbnailsEl.id = "thumbnails";
-carouselEl.append(thumbnailsEl);
-
-for(let i = 0; i < images.length; i++) {
-    const thumbnailEl = document.createElement("div");
-    thumbnailEl.classList.add("thumbnail");
-    thumbnailEl.style.height = "calc(100% / " + images.length + ")";
-    thumbnailsEl.append(thumbnailEl);
-
-    const thumbnailImageEl = document.createElement("img");
-    thumbnailImageEl.src = "../" + images[i].image;
-    thumbnailEl.append(thumbnailImageEl);
-}
-
-const controlPrevEl = document.createElement("i");
-controlPrevEl.id = "control-prev";
-controlPrevEl.className = "fa-solid fa-circle-chevron-up fa-2xl";
-thumbnailsEl.append(controlPrevEl);
-
-const controlNextEl = document.createElement("i");
-controlNextEl.id = "control-next";
-controlNextEl.className = "fa-solid fa-circle-chevron-down fa-2xl";
-thumbnailsEl.append(controlNextEl);
-
-const thumbnailsList = document.querySelectorAll(".thumbnail");
-thumbnailsList.forEach((actualThumbnail, thumbnailIndex) => {
-    actualThumbnail.addEventListener("click", () => {
-        actualIndex = thumbnailIndex;
-        showImageByIndex(actualIndex);
-    });
-});
-
-let actualIndex = 0;
-showImageByIndex(actualIndex);
-
-let isReverseRotation = false;
-let nextImageInterval = setInterval(showNextImage, 3000);
-let isIntervalActive = true;
-
-
-controlPrevEl.addEventListener("click", showPrevImage);
-
-
-controlNextEl.addEventListener("click", showNextImage);
-
-
 controlStartEl.addEventListener("click", () => {
     if(!isIntervalActive) {
         nextImageInterval = setInterval(() => {
@@ -125,6 +118,12 @@ controlStartEl.addEventListener("click", () => {
 });
 
 
+
+// Creo il bottone per fermare lo switch automatico del carosello ogni 3 secondi
+const controlStopEl = document.createElement("i");
+controlStopEl.id = "control-stop";
+controlStopEl.className = "fa-solid fa-circle-pause fa-2xl";
+mainPictureEl.append(controlStopEl);
 controlStopEl.addEventListener("click", () => {
     if(isIntervalActive){
         clearInterval(nextImageInterval);
@@ -136,6 +135,11 @@ controlStopEl.addEventListener("click", () => {
 });
 
 
+// Creo il bottone per cambiare il senso di rotazione automatica del carosello
+const controlChangeEl = document.createElement("i");
+controlChangeEl.id = "control-change";
+controlChangeEl.className = "fa-solid fa-arrows-rotate fa-2xl";
+mainPictureEl.append(controlChangeEl);
 controlChangeEl.addEventListener("click", () => {
     isReverseRotation = !isReverseRotation;
 
@@ -155,8 +159,12 @@ controlChangeEl.addEventListener("click", () => {
 });
 
 
-
-
+// Inizializzo il carosello
+let isReverseRotation = false;
+let nextImageInterval = setInterval(showNextImage, 3000);
+let isIntervalActive = true;
+let actualIndex = 0;
+showImageByIndex();
 
 // ------------------- FUNCTIONS ------------------- //
 
@@ -165,23 +173,23 @@ function showPrevImage() {
     actualIndex += images.length;
     actualIndex %= images.length;
 
-    showImageByIndex(actualIndex);
+    showImageByIndex();
 }
 
 function showNextImage() {
     actualIndex++;
     actualIndex %= images.length;
 
-    showImageByIndex(actualIndex);
+    showImageByIndex();
 }
 
-function showImageByIndex(i) {
-    mainPictureImageEl.src = "../" + images[i].image;
-    mainPictureTitleEl.innerText = images[i].title;
-    mainPictureTextEl.innerText = images[i].text;
+function showImageByIndex() {
+    mainPictureImageEl.src = "../" + images[actualIndex].image;
+    mainPictureTitleEl.innerText = images[actualIndex].title;
+    mainPictureTextEl.innerText = images[actualIndex].text;
 
     thumbnailsList.forEach((currentThumbnail, index) => {
-        if(index == i) {
+        if(index == actualIndex) {
             currentThumbnail.classList.add("active");
         }
         else {
